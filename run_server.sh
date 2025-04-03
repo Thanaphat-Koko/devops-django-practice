e3e790a7a5956baa434a57e4218f98e67b7fdbfa
Thanaphat-Koko <thanaphat.peth@gmail.com>
2025-04-03 09:13:35 +0000

    edit run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,9 @@
 
 echo "Show library list................................."
 poetry show
+echo "Check environment info............................"
+poetry env info
+poetry show gunicorn
 echo "Run Server........................................"
+poetry run which gunicorn
 poetry run gunicorn project_jrd.wsgi:application --bind 0.0.0.0:8000
